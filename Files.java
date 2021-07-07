e6b8ea0c8f12141c6be80d4938924c9d9bb6ed9f
amith0608 <85832517+amith0608@users.noreply.github.com>
2021-07-07 13:58:44 +0530

    Create Files.java

---
+++ Files.java
@@ -0,0 +1,44 @@
+import java.io.*;
+public class Files
+{
+  public static void main(String[] args)throws Exception
+  {
+	  //To write in a file
+	  try {
+	      FileWriter myWriter = new FileWriter("E:\\Amith\\amith.txt");
+	      myWriter.write("This is File handling in Java");
+	      myWriter.close();
+	      System.out.println("Successfully wrote to the file.");
+	    } catch (IOException e) {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	    }
+	  
+	  //To read from a file
+  File file = new File("E:\\Amith\\amith.txt");
+  
+  BufferedReader br = new BufferedReader(new FileReader(file));
+  
+  String st;
+  while ((st = br.readLine()) != null)
+    System.out.println(st);
+  
+  
+  	//To append to a file
+  try {
+      String data = "Java is the best language";
+      File f1 = new File("E:\\Amith\\amith.txt");
+      if(!f1.exists()) {
+         f1.createNewFile();
+      }
+
+      FileWriter fileWritter = new FileWriter(f1.getName(),true);//appending
+      BufferedWriter bw = new BufferedWriter(fileWritter);
+      bw.write(data);
+      bw.close();
+      System.out.println("Append Done");
+   } catch(IOException e){
+      e.printStackTrace();
+   }
+  }
+}
